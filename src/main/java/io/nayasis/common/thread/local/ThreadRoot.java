689304b1c8f43e95234638a7ba9c0b94dbe3647f
nayasis <nayasis@gmail.com>
2019-02-28 17:15:00 +0900

    add ThreadLocal.

---
+++ src/main/java/io/nayasis/common/thread/local/ThreadRoot.java
@@ -0,0 +1,38 @@
+package io.nayasis.common.thread.local;
+
+import java.util.Observable;
+import java.util.Observer;
+import java.util.UUID;
+
+public class ThreadRoot implements Observer {
+
+	public static final String WATCHER_KEY = ThreadRoot.class.getName() + ".WATCHER_KEY";
+
+	private static final java.lang.ThreadLocal threadRoot = new InheritableThreadLocal<String>() {
+
+		public String initialValue() {
+	        return UUID.randomUUID().toString();
+	    }
+
+		protected String childValue( String parentValue ) {
+	    	return parentValue == null || parentValue.length() == 0 ? UUID.randomUUID().toString() : parentValue;
+	    }
+
+	};
+
+	static {
+		ThreadLocal.addObserver( new ThreadRoot() );
+	}
+
+	@Override
+	public void update( Observable watcher, Object deliveredParameter ) {
+		if( ! WATCHER_KEY.equals( deliveredParameter ) ) return;
+		threadRoot.remove();
+	}
+
+	public static String getKey() {
+		return (String) threadRoot.get();
+	}
+
+
+}
