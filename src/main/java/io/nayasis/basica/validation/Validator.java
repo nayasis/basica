55d2bb7d9a5c89a7eb8a93c642cdb1f6ed32ff08
nayasis <nayasis@gmail.com>
2020-04-02 15:41:32 +0900

    add null safe equality check method.

--- src/main/java/io/nayasis/basica/validation/Validator.java
+++ src/main/java/io/nayasis/basica/validation/Validator.java
@@ -10,6 +10,7 @@ import io.nayasis.basica.reflection.Reflector;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -381,6 +382,52 @@ public class Validator {
      */
     public static boolean isJson( String text ) {
         return Reflector.isJson( text );
+    }
+
+    /**
+     * determine if the given objects are equal.
+     * <ul>
+     *   <li>it is null safe.</li>
+     *   <li>it can check equality between arrays.</li>
+     * </ul>
+     *
+     * @param o1    object
+     * @param o2    another object
+     * @return true if the given objects are equal.
+     */
+    public static boolean isEqual( Object o1, Object o2 ) {
+        if( o1 == o2 )
+            return true;
+        if( o1 == null || o2 == null )
+            return false;
+        if( o1.equals(o2) )
+            return true;
+        if( o1.getClass().isArray() && o2.getClass().isArray() ) {
+            return arrayEquals(o1, o2);
+        }
+        return false;
+    }
+
+    private static boolean arrayEquals( Object o1, Object o2 ) {
+        if( o1 instanceof Object[] && o2 instanceof Object[] )
+            return Arrays.equals((Object[]) o1, (Object[]) o2);
+        if( o1 instanceof boolean[] && o2 instanceof boolean[] )
+            return Arrays.equals((boolean[]) o1, (boolean[]) o2);
+        if( o1 instanceof byte[] && o2 instanceof byte[] )
+            return Arrays.equals((byte[]) o1, (byte[]) o2);
+        if( o1 instanceof char[] && o2 instanceof char[] )
+            return Arrays.equals((char[]) o1, (char[]) o2);
+        if( o1 instanceof double[] && o2 instanceof double[] )
+            return Arrays.equals((double[]) o1, (double[]) o2);
+        if( o1 instanceof float[] && o2 instanceof float[] )
+            return Arrays.equals((float[]) o1, (float[]) o2);
+        if( o1 instanceof int[] && o2 instanceof int[] )
+            return Arrays.equals((int[]) o1, (int[]) o2);
+        if( o1 instanceof long[] && o2 instanceof long[] )
+            return Arrays.equals((long[]) o1, (long[]) o2);
+        if( o1 instanceof short[] && o2 instanceof short[] )
+            return Arrays.equals((short[]) o1, (short[]) o2);
+        return false;
     }
 
 }
